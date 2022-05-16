8fe5d828b2b242de17b20aedfb3c9ad3b475bbda
biswa-ranjan-subudhi <biswaranjan.cuh@gmail.com>
2022-05-16 20:36:20 +0530

    Intial Commit

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,21 +1,21 @@
 export const educationData = [{
         id: 1,
         institution: 'Masai School, Bangalore',
-        course: 'Full Stack Web Development Course',
+        course: 'Front End Web Development Course',
         startYear: '2021',
         endYear: 'Present'
     },
     {
         id: 2,
         institution: 'Central University of Haryana, Haryana',
-        course: 'Bachelor of Technology, Electrical Engineering',
+        course: 'B.Tech in Electrical Engineering',
         startYear: '2018',
         endYear: 'Present'
     },
     {
         id: 3,
         institution: 'Ispat English Medium School, Rourkela',
-        course: 'Mathematics and Computer Science (Indian School Certificate)',
+        course: 'Mathematics and Computer Science (ISC)',
         startYear: '2016',
         endYear: '2018'
     },
